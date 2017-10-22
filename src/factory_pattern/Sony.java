ecbecc8efe11a062f8567326aa3f48c5e86abbe8
Natnael Alemayehu <dinksira@gmail.com>
2017-10-21 20:47:56 -0500

    Update Sony.java

--- src/factory_pattern/Sony.java
+++ src/factory_pattern/Sony.java
@@ -1,10 +1,3 @@
-/* ************************************************************************************
- * Copyright (C) Oct 21, 2017, Natnael Alemayehu, All rights Reserved. Unauthorized 
- * copying of this file and/or distributed without, the express permission of 
- * Natnael Alemayehu is strictly prohibited. Written by Natnael Alemayehu 6:28:52 PM.
- * ************************************************************************************
- */
-
 package factory_pattern;
 
 /**
