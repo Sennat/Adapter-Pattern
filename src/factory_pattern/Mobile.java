818ed98a31f0b4c6dc16d385fa2b0d50b0f2ba56
Natnael Alemayehu <dinksira@gmail.com>
2017-10-21 20:46:28 -0500

    Update Mobile.java

--- src/factory_pattern/Mobile.java
+++ src/factory_pattern/Mobile.java
@@ -1,10 +1,3 @@
-/* ************************************************************************************
- * Copyright (C) Oct 21, 2017, Natnael Alemayehu, All rights Reserved. Unauthorized 
- * copying of this file and/or distributed without, the express permission of 
- * Natnael Alemayehu is strictly prohibited. Written by Natnael Alemayehu 5:32:28 PM.
- * ************************************************************************************
- */
-
 package factory_pattern;
 
 /**
